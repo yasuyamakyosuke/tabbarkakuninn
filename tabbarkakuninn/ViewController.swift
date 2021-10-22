d895d5392ed19901750319a7b98be3ecd29eeaf7
Yasuyamakyosuke <toyojindai@gmail.com>
2021-10-22 14:51:29 +0900

    kakuninn

---
+++ tabbarkakuninn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabbarkakuninn
+//
+//  Created by 泰山恭輔 on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
